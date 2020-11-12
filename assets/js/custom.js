416b41b6e5a767cd336e04ce93fd61029836e274
Ishan Vyas <vyasishanatc194@gmail.com>
2020-11-12 09:52:40 +0530

    working in dashboard

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,33 +1,43 @@
-/* Set navigation */
-
-function openNav() {
-  $("#mySidenav").addClass("width80");
-  $("#nav-res").addClass("opacityon");
-  $(".cd-shadow-layer").addClass("displayblock");
-  $(".wrapper").addClass("position-fixed-custom");
-  $("body").addClass("overflow-fixed");
-}
-
-function closeNav() {
-  $("#mySidenav").removeClass("width80");
-  $("#nav-res").removeClass("opacityon");
-  $(".cd-shadow-layer").removeClass("displayblock");
-  $(".wrapper").removeClass("position-fixed-custom");
-  $("body").removeClass("overflow-fixed");
-} 
-
 $(document).ready(function(){ 
 
-  $(".cd-shadow-layer").click(function(){
-    closeNav(); 
+  /* mobile menu */
+  $("#menu-open").click(function(){
+    $("#dashboard-menu-clone").addClass("active");
+    $(".left-side-nav").addClass("active");
+    $(".backdrop-menu-layer").addClass("active");
+    $(".nav-dashboard-right").addClass("active");
   });
 
-  $(window).scroll(function(){
-    var sticky = $(".header-div"),
-        scroll = $(window).scrollTop();
-
-      if (scroll >= 190) sticky.addClass("header-bgcolor slideInDown animated");
-      else sticky.removeClass("header-bgcolor slideInDown animated");
-
+  $("#menu-closed").click(function(){
+    $("#dashboard-menu-clone").removeClass("active");
+    $(".left-side-nav").removeClass("active");
+    $(".backdrop-menu-layer").removeClass("active");
+    $(".nav-dashboard-right").removeClass("active");
   });
+
+  $(".backdrop-menu-layer").click(function(){
+    $("#dashboard-menu-clone").removeClass("active");
+    $(".left-side-nav").removeClass("active");
+    $(".backdrop-menu-layer").removeClass("active");
+    $(".nav-dashboard-right").removeClass("active");
+  });
+  
+  /* End of mobile menu */
+
+  /* Desktop collapse menu */
+  $("#collapse-nav").click(function(){
+    $(this).toggleClass("collapsed-menu");
+    $(".left-side-nav").toggleClass("collapse-menu");
+    $(".header-dashboard").toggleClass("collapsed-header");
+    $(".main-dashboard").toggleClass("dashboard-expand");
+    $("body").toggleClass("mini-navbar");
+    $(".left-side-menu").addClass("text-fixed-remove");
+    setTimeout(function() {
+      $(".left-side-menu").removeClass("text-fixed-remove");
+    }, 200);
+  });
+  /* End of desktop collapse menu */
+
+  $("[data-toggle='tooltip']").tooltip();
+
 });
